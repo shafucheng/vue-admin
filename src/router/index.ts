5ce406a3ca88e98ffa18649106a9fb7cafabf525
Sha Fucheng <sha0329@outlook.com>
2022-11-02 00:25:03 +0800

    feat: auto set route name

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,19 +15,30 @@ const routes: RouteRecordRaw[] = [
     children: [
       {
         path: 'home',
-        name: Symbol(),
         meta: { title: 'Home' },
         component: () => import('@/views/HomeView'),
       },
       {
         path: 'about',
-        name: Symbol(),
         meta: { title: 'About' },
         component: () => import('@/views/AboutView'),
       },
     ],
   },
 ]
+
+const recursionRoutes = (routes: RouteRecordRaw[]) => {
+  routes.map((route) => {
+    if (typeof route.name === 'undefined') {
+      route.name = Symbol()
+    }
+    if (route.children) {
+      recursionRoutes(route.children)
+    }
+  })
+}
+
+recursionRoutes(routes)
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
