161dd81a71cdd2478ba58f24e40fccf819b405ce
Sha Fucheng <sha0329@outlook.com>
2022-11-05 02:32:37 +0800

    fix: SearchForm submit

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,5 +1,6 @@
 import { Form } from '@formily/antdv-x3'
 import { createForm } from '@formily/core'
+import _ from 'lodash-es'
 import { defineComponent, onMounted } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 
@@ -27,7 +28,7 @@ export default defineComponent({
     const form = createForm()
 
     const onSubmit = (querySync = true) => {
-      const values = form.values
+      const values = _.cloneDeep(form.values)
       if (querySync) {
         const encodeValues: any = {}
         Object.keys(values).forEach((key) => {
@@ -42,14 +43,14 @@ export default defineComponent({
           }
         })
         router.replace({
-          query: Object.assign({}, route.query, encodeValues),
+          query: _.assign({}, _.cloneDeep(route.query), encodeValues),
         })
       }
       emit('submit', values)
     }
 
     onMounted(() => {
-      const query = route.query
+      const query = _.cloneDeep(route.query)
       const decodeQuery: any = {}
       Object.keys(query).forEach((key) => {
         try {
